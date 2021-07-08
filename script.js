f0c62c11a15005c4ad90db7a4e7159e47f799d02
SteveNoyes <stevenmnoyes.af@gmail.com>
2021-07-08 16:51:13 -0500

    rework0

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function myFunction() {
+function menuIcon() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
     x.className = "topnav";
   }
+}
+
+function activeMenu() {
+  var y = document.getElementsByClassName("active");
 }
